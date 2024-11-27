a55376fe6cfe5a5201522caccd3bea07e576d65a
Flavia-Sa <flaviaellen98@gmail.com>
2024-11-27 16:50:35 -0300

    adicionei e exclui tabelas

--- contas/forms.py
+++ contas/forms.py
@@ -1,11 +1,24 @@
-# contas/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Usuario, Financas_pessoas, Informacoes_bancarias, Vendas, Pequeno_negocio
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+class UsuarioForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']  # Inclui somente o que você deseja
+        model = Usuario
+        fields = ['email', 'nome', 'cpf', 'data_nascimento', 'senha']
+class Financas_pessoasForm(forms.ModelForm):
+     class Meta:
+        model = Financas_pessoas
+        fields = ['renda_mensal', 'gastos_fixos_mensais', 'gastos_variaveis_mensais', 'valor_investimentos', 'valor_dividas', 'valor_objeto_financeiro']
+class Informacoes_bancariasForm(forms.ModelForm):
+    class Meta:
+        model = Informacoes_bancarias
+        fields = ['banco', 'agencia', 'conta', 'codigo_confirmacao']
+class VendasForm(forms.ModelForm):
+    class Meta:
+        model = Vendas
+        fields = ['nome_produto', 'quantidade_de_produto_vendido', 'preco', 'data_da_venda', 'status']
+class Pequeno_negocioForm(forms.ModelForm):
+    class Meta:
+        model = Pequeno_negocio
+        fields = ['razao_social', 'responsavel', 'vategoria', 'renda_mensal', 'despesas_fixas', 'despesas_variaveis']
+
--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from datetime import datetime #importando data atual
 
-# Create your models here.
+class Usuario(models.Model):
+    email = models.EmailField()
+    nome = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DataField(max_length=15)
+    senha = models.CharField()
+
+
+class Financas_pessoas(models.Model):
+    renda_mensal = models.DecimalField()
+    gastos_fixos_mensais = models.DecimalField()
+    gastos_variaveis_mensais = models.DecimalField()
+    valor_investimentos = models.DecimalField()
+    valor_dividas = models.DecimalField()
+    valor_objeto_financeiro = models.DecimalField()
+    #index fk_usuario
+
+class Informacoes_bancarias(models.Model):
+    banco = models.IntergerField()
+    agencia = models.IntergerField()
+    conta = models.IntergerField()
+    saldo = models.DecimalField()
+    codigo_confirmacao = models.IntergerField()
+    #index fk_usuario
+    #index fk_pequeno_negocio
+
+#Novos
+class Vendas(models.Model):
+    nome_produto = models.CharField(max_length=150)
+    quantidade_de_produto_vendido = models.IntergerField()#quantidade do um produto vendido numa venda expecifica
+    preco = models.DecimalField()
+    data_da_venda = models.date.today()
+    status = models.CharField(max_length=50)
+    #index fk_pequeno_negocio
+
+class Pequeno_negocio(models.Model):
+    razao_social = models.CharField(max_length=100)
+    responsavel = models.CharField(max_length=45)
+    vategoria = models.CharField(max_length=50)
+    renda_mensal = models.DecimalField()
+    despesas_fixas = models.DecimalField()
+    despesas_variaveis = models.DecimalField()
+    #index fk_Financas_pessoas
