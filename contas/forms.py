a43d681ce24ad14f921a861d3a11995944ecc07f
Flavia-Sa <flaviaellen98@gmail.com>
2024-11-27 13:39:28 -0300

    feat(auth): adicionando models e forms

--- contas/forms.py
+++ contas/forms.py
@@ -1,11 +1,19 @@
-# contas/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Cadastrar, Controle_financeiro, Informacoes_bancarias, Controle_de_vendas
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+class CadastrarForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']  # Inclui somente o que você deseja
+        model = Cadastrar
+        fields = ['email', 'nome', 'cpf', 'data_nascimento', 'senha']
+class Controle_financeiroForm(forms.ModelForm):
+     class Meta:
+        model = Controle_financeiro
+        fields = ['renda_mensal', 'gastos_fixos_mensais', 'gastos_variaveis_mensais', 'valor_investimentos', 'valor_dividas', 'valor_objeto_financeiro']
+class Informacoes_bancariasForm(forms.ModelForm):
+    class Meta:
+        model = Informacoes_bancarias
+        fields = ['banco', 'agencia', 'conta', 'codigo_confirmacao']
+class Controle_de_vendasForm(forms.ModelForm):
+    class Meta:
+        model = Controle_de_vendas
+        fields = ['valor_total_vendas_dia', 'quantidade_total_vendas', 'estoque_atual', 'lucro_estimado', 'nome_produto', 'quantidade_de_produto_vendido']
--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Cadastrar(models.Model):
+    email = models.EmailField()
+    nome = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DataField(max_length=15)
+    senha = models.CharField()
+
+
+class Controle_financeiro(models.Model):
+    renda_mensal = models.DecimalField()
+    gastos_fixos_mensais = models.DecimalField()
+    gastos_variaveis_mensais = models.DecimalField()
+    valor_investimentos = models.DecimalField()
+    valor_dividas = models.DecimalField()
+    valor_objeto_financeiro = models.DecimalField()
+
+class Informacoes_bancarias(models.Model):
+    banco = models.CharField(max_length=150)
+    agencia = models.IntergerField()
+    conta = models.IntergerField()
+    codigo_confirmacao = models.IntergerField()
+
+class Controle_de_vendas(models.Model):
+    valor_total_vendas_dia = models.DecimalField()
+    quantidade_total_vendas = models.IntergerField()#quantidade total de vendas por todos os produtos
+    estoque_atual  = models.IntergerField()
+    lucro_estimado = models.DecimalField()
+    nome_produto = models.CharField(max_length=150)
+    quantidade_de_produto_vendido = models.IntergerField() #quantidade do um produto vendido numa venda expecifica
