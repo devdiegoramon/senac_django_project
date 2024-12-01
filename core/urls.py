b41f480e3cf25853cc6ac7522edd8c07fabd0f2c
CawanBr7 <cawanbr7@gmail.com>
2024-11-30 22:04:50 -0300

    ajustando os htmls css e telas

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,16 @@ urlpatterns = [
     path('controle.f/', views.controlef_view, name='controle.f'),
     path('pnegocios/', views.pnegocios_view, name='pnegocios'),
     path('recsenha/', views.recsenha_view, name='recsenha'),
+    path('suporte/',views.suporte_view, name='suporte'),
+    path('inicio2/', views.inicio2_view, name='inicio2'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('adicionar_c/', views.adicionar_c, name='adicionar_c'),
+    path('banco/', views.banco, name='banco'),
+    path('altsenha/', views.altsenha, name='altsenha'),
+    path('altemail/', views.altemail, name='altemail'),
+    path('alteraemail1/', views.altemail, name='altemail1'),
+    path('controle.f2/', views.controlef_view, name='controle.f2'),
+    path('erro/', views.erro, name='erro'),
+    path('pnegocios2/', views.pnegocios2, name='pnegocios2'),
 ]
 
--- core/views.py
+++ core/views.py
@@ -20,3 +20,35 @@ def pnegocios_view(request):
 def recsenha_view(request):
     return render(request, 'core/recsenha.html')
 
+def suporte_view(request):
+    return render(request, 'core/suporte.html')
+
+def inicio2_view(request):
+    return render(request, 'core/inicio2.html')
+
+def perfil(request):
+    return render(request, 'core/perfil.html')
+
+def adicionar_c(request):
+    return render(request, 'core/adicionar_c.html')
+
+def banco(request):
+    return render(request, 'core/banco.html')
+
+def altsenha(request):
+    return render(request, 'core/altsenha.html')
+
+def altemail(request):
+    return render(request, 'core/altemail.html')
+
+def alteraemail1(request):
+    return render(request, 'core/alteraemail1.html')
+
+def controlef2_view(request):
+    return render(request, 'core/controle.f2.html')
+
+def erro(request):
+    return render(request, 'core/erro.html')
+
+def pnegocios2(request):
+    return render(request, 'core/pnegocios2.html')
